34e58e193ffa3636a6e729797fd510bfb3bae41f
raselahmed85 <mr.raselahmed85@gmail.com>
2025-03-10 09:49:06 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,53 @@
+
+
+// const products = [
+//     {
+//         id : 1,
+//         name : "Laptop",
+//         price : 1000,
+//         Image:"/all_proudct_images/415f1dd5c2f2a7ee69f954b8d9d6f6ce.webp"
+//     },
+//     {
+//         id : 1,
+//         name : "Laptop",
+//         price : 1000,
+//         Image:"/all_proudct_images/ajwa.webp"
+//     },
+//     {
+//         id : 1,
+//         name : "Laptop",
+//         price : 1000,
+//         Image:"/all_proudct_images/akroat-03_336fcd32-d311-4d2c-b87c-9b81a1dea9f2.webp"
+//     },
+//     {
+//         id : 1,
+//         name : "Laptop",
+//         price : 1000,
+//         Image:"/all_proudct_images/Black-Cumin.webp"
+//     },
+//     {
+//         id : 1,
+//         name : "Laptop",
+//         price : 1000,
+//         Image:"/all_proudct_images/chola-dal.webp"
+//     },
+// ]
+
+
+
+// let productCon = document.getElementById("productContainer")
+
+// for (let i = 0; i < products.length; i++) {
+
+//     const product = products[i];
+    
+//     productCon.innerHTML+= `
+//     <div>
+//     <img src="${product.Image}" alt="">
+//     <p>Name :${product.name}</p>
+//     <p>Price :${product.price} </p>
+    
+//     </div>
+//     `
+    
+// }
